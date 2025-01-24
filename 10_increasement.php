db31fc5c92c3d81c8d3b939e107dabf4a3b1cd3b
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-01-24 11:57:16 +0530

    Increement Operator

---
+++ 10_increasement.php
@@ -0,0 +1,12 @@
+<?php
+
+// increement
+$counter = 0;
+$counter++; // increment by 1
+$counter += 1; // increment by 1
+$counter++; // increment by 1
+// output: 3
+$counter += 1;
+$counter++;
+echo ($counter);
+?>
