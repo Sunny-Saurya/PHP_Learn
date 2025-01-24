264dd576700ab01d58326aae56d6dde5234fb7af
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-01-24 11:54:32 +0530

    Additon in PHP

---
+++ 03_addition.php
@@ -0,0 +1,26 @@
+<?php
+ $a = '20';
+ $b = '16';
+ $c = $a + $b;
+ echo $c ."<br>";
+
+ $data1 = "PHP";
+ echo "I am learning $data1 ! <br>";
+
+ $data1 = "PHP";
+ echo "I am leaning ". $data1 . "!";
+
+ // php integer
+ $number = 11;
+ var_dump($number);
+ 
+ // php float
+ $x = 18.27;
+ var_dump($x); 
+ 
+
+ // php boolera
+ $x = True;
+ var_dump($x);
+
+?>
