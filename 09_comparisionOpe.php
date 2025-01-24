ede818f649d765c0694fa72c6bb65b1709fdeeb6
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-01-24 11:57:48 +0530

    comparision operator

---
+++ 09_comparisionOpe.php
@@ -0,0 +1,23 @@
+<?php
+// comparision operation
+
+$a = 10;
+$b = 20;
+
+var_dump($a == $b);
+var_dump($b == $a);
+var_dump($a == $b);
+var_dump($b == $a);
+var_dump($b == $b);
+var_dump($a == $b);
+var_dump($b == $a);
+var_dump($b == $b);
+var_dump($a !== $b);
+var_dump($b !== $a);
+var_dump($b !== $b);
+var_dump($a !== $b);
+var_dump($b !== $a);
+
+echo($a == $b);
+
+?>
