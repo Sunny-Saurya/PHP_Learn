a8a85e83fd3317b963c1352bcee3a8dde41060ea
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-01-31 12:39:47 +0530

    switch case

---
+++ 16_switchCase.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 1;
+switch ($a) {
+    case 1:
+        echo"This is case 1";
+break;
+case 2:
+    echo "This is case 2";
+    break;
+    case 3:
+        echo "This is case 3";
+        break;
+    default:
+    echo "This is default case";
+}
+?>
