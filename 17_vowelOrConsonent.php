6a2ee7e4f188e345abcbe5b49630ea0f2134923d
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-01-31 12:49:43 +0530

    Vowel or Consonent

---
+++ 17_vowelOrConsonent.php
@@ -0,0 +1,24 @@
+<?php
+$a = 'a';
+
+switch ($a) {
+    case 'a':
+        echo'This is Vowel';
+        break;
+    case 'e':
+        echo 'This is vowel';
+        break;
+    case 'i':
+        echo 'This is vowel';
+        break;
+    case 'o':
+        echo 'This is vowel';
+        break;
+    case 'u':
+        echo 'This is vowel';
+        break;
+
+    default:
+    echo 'This is Consonent';
+    break;
+}
