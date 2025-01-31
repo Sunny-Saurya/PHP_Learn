ef751e81d04f1bd96475904ae344239879c6acc9
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-01-31 12:12:32 +0530

    Grade Dist

---
+++ 14_gradeDist.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 90;
+
+// grade assigning
+if ($a >= 80 && $a <= 100) {
+    echo " Grade A";
+}
+else if( $a >= 70&& $a <= 80){
+    echo "Grade B";
+}
+else if( $a >= 50&& $a <= 70){
+    echo "Grade C";
+}
+else if( $a >= 40&& $a <= 50){
+    echo "Grade D";
+}
+else{
+    echo "Sorry You're Fail";
+}
