3882bbd97255d3bcc633ae10b2b63c39911f9f79
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-01-31 12:23:15 +0530

    new file

--- 14_gradeDist.php
+++ 14_gradeDist.php
@@ -1,20 +1,22 @@
 <?php
 
-$a = 90;
+$a = 30;
 
 // grade assigning
 if ($a >= 80 && $a <= 100) {
     echo " Grade A";
 }
-else if( $a >= 70&& $a <= 80){
+else if( $a >= 70 && $a <= 80){
     echo "Grade B";
 }
-else if( $a >= 50&& $a <= 70){
+else if( $a >= 50 && $a <= 70){
     echo "Grade C";
 }
-else if( $a >= 40&& $a <= 50){
+else if( $a >= 40 && $a <= 50){
     echo "Grade D";
 }
 else{
     echo "Sorry You're Fail";
 }
+
+?>
