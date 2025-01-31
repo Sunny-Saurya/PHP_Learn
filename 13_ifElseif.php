37c976b686e99429e78444a77ce0e4dd82d98e79
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-01-31 12:07:56 +0530

    If else if

---
+++ 13_ifElseif.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 20;
+$b = 10;
+$c = 30;
+
+if($a > $b && $a > $c){
+    echo "$a is the Maximum number among these 3";
+}
+else if($b > $a && $b > $c){
+    echo "$b is the maximum number among these 3";
+}
+else{
+    echo " $c is the maximum number amaong these 3";
+}
