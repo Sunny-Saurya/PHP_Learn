601027a1bd74bf3d709c50977cb5e9c8782a7596
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-02-05 11:24:26 +0530

    for_each

---
+++ 19_for_each.php
@@ -0,0 +1,26 @@
+<?php
+$superman = array(
+    "name"=> "Peter parmer",
+    "email"=> "peter@gmail.com",
+    "age"=> "18",
+);
+
+foreach($superman as $key => $value){
+    echo"".$key." : ".$value."<br>";
+}
+?>
+
+<?php
+
+$myDetails = array(
+    "name" => "Sunny",
+    "age" => 20,
+    "email" => "sunnysaurya9955@gmail.com",
+    "height" => "5'7",
+    "education" => "B.Tech",
+);
+
+foreach($myDetails as $key => $value){
+    echo "".$key." : ".$value."<br>";
+}
+?>
