7b7684f171872309b0b1b5e16b5dbf225a8db854
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-02-19 07:01:17 +0530

    Require and INclude

---
+++ 36_require.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require '35_armstrong.php';
+        require 'Activity1.php';
+        require 'Activity2.php';
+    ?>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quisquam at repellat?
+    </p>
+</body>
+</html>
