3ee5adcb09b4acc49b687ffc8e23e056bd8143ca
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-02-26 23:16:11 +0530

    new file

--- hello.php
+++ hello.php
@@ -1,3 +1,16 @@
-<?php
-echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="41_validation.php" method="POST">
+        <input type="text" name="name" placeholder="Username">
+        <input type="text" name="email" placeholder="email">
+        <button>Submit</button>
+    </form>
+
+</body>
+</html>
